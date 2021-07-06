50a4bbecae120d4b36b4d92ef7cbcc0fda7ea630
unknown <a-f-r@hotmail.es>
2021-07-06 23:41:49 +0200

    Display error message when there's an error

--- src/App.js
+++ src/App.js
@@ -7,13 +7,17 @@ import LoadingSpinner from './components/LoadingSpinner';
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const onFetchMoviesHandler = async function () {
     try {
       setIsLoading(true);
+      setError(null);
       const res = await fetch('https://swapi.dev/api/films');
       const data = await res.json();
-      if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+      if (!res.ok) {
+        throw new Error(`${data.message} (${res.status})`);
+      }
       const transformedMovies = data.results.map((movie) => {
         return {
           id: movie.episode_id,
@@ -27,6 +31,8 @@ function App() {
       setIsLoading(false);
     } catch (err) {
       console.error(err);
+      setIsLoading(false);
+      setError(err);
     }
   };
 
@@ -38,6 +44,7 @@ function App() {
       <section>
         {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
         {isLoading && <LoadingSpinner />}
+        {error && <p>Something went wrong</p>}
       </section>
     </React.Fragment>
   );
